502b0510ac55532ed6024e1caae9a3dbe1fac751
shubhamspashte <143743743+shubhamspashte@users.noreply.github.com>
2024-02-02 20:53:12 +0530

    change the navbar color

--- raise/src/index.js
+++ raise/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-// import Image from './image';
+import Image from './image';
 import Navbar from './navbar';
 import  Footer from './footer';
-// import Main from './main';
-import Hmain from './hmain';
+import Main from './main';
+// import Hmain from './hmain';
 // import LoginPage from './login';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -36,9 +36,9 @@ import Hmain from './hmain';
 ReactDom.render(
         <React.StrictMode>
                 <Navbar/>
-                { <Hmain/> }
-                {/* <Image/> */}
-                {/* <Main/>/ */}
+                {/* <Hmain/> */}
+                <Image/>
+                <Main/>/
                 <Footer/>
                 {/* <LoginPage/> */}
         </React.StrictMode>, 
